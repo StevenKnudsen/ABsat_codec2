add4c9d0f9c308bf587ae65956957fe9ab62f5eb
khorassa <43244287+khorassa@users.noreply.github.com>
2022-05-31 11:37:04 -0600

    Necessary information added

--- codec2.py
+++ codec2.py
@@ -1,15 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# This script was inspired by a flowgraph found in gnuradio's github repo. 
+# The flowgraph was created by Martin Braun and is linked below:
+# https://github.com/gnuradio/gnuradio/blob/master/gr-vocoder/examples/loopback-codec2.grc
 
-#
-# SPDX-License-Identifier: GPL-3.0
-#
-# GNU Radio Python Flow Graph
-# Title: Codec2 Looback Test
-# Author: Martin Braun
-# Copyright: 2014,2019 Free Software Foundation, Inc.
-# Description: An example how to use the Codec2 Vocoder
-# GNU Radio version: 3.9.4.0
+# Title: Codec2 encoder and decoder
+# Author: Shayan Khorassany
+# Description: A script that could encode or decode audio recordings using command line arguments. 
+#              It was created under the guidance of Dr. Steven Knudsen to be used in AlbertaSat
+
 
 from distutils.version import StrictVersion
 
@@ -24,24 +21,12 @@ if __name__ == '__main__':
             print("Warning: failed to XInitThreads()")
 
 from PyQt5 import Qt
-from gnuradio import qtgui
-from gnuradio.filter import firdes
-import sip
-from gnuradio import audio
 from gnuradio import blocks
 from gnuradio import filter
 from gnuradio import gr
-from gnuradio.fft import window
 import sys, getopt
-import signal
-from argparse import ArgumentParser
-from gnuradio.eng_arg import eng_float, intx
-from gnuradio import eng_notation
 from gnuradio import vocoder
 from gnuradio.vocoder import codec2
-
-
-
 from gnuradio import qtgui
 
 class loopback_codec2(gr.top_block):
@@ -52,38 +37,9 @@ class loopback_codec2(gr.top_block):
 
     def __init__(self, mode, audio_file="people_call_me_steve_Au48k.wav", samp_rate=48000, bit_rate=2400, output_filename='sample_output'): 
         gr.top_block.__init__(self, "Codec2 Looback Test", catch_exceptions=True)
-        # Qt.QWidget.__init__(self)
-        # self.setWindowTitle("Codec2 Looback Test")
-        # qtgui.util.check_set_qss()
-        # try:
-        #     self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        # except:
-        #     pass
-        # self.top_scroll_layout = Qt.QVBoxLayout()
-        # self.setLayout(self.top_scroll_layout)
-        # self.top_scroll = Qt.QScrollArea()
-        # self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        # self.top_scroll_layout.addWidget(self.top_scroll)
-        # self.top_scroll.setWidgetResizable(True)
-        # self.top_widget = Qt.QWidget()
-        # self.top_scroll.setWidget(self.top_widget)
-        # self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        # self.top_grid_layout = Qt.QGridLayout()
-        # self.top_layout.addLayout(self.top_grid_layout)
-
-        # self.settings = Qt.QSettings("GNU Radio", "loopback_codec2")
-
-        # try:
-        #     if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-        #         self.restoreGeometry(self.settings.value("geometry").toByteArray())
-        #     else:
-        #         self.restoreGeometry(self.settings.value("geometry"))
-        # except:
-        #     pass
 
         ##################################################
         # Variables
-        ##################################################
         self.mode = mode
         self.scale = scale = 2**13
         self.samp_rate = samp_rate
@@ -95,21 +51,7 @@ class loopback_codec2(gr.top_block):
         self.buf_size = 0
         self.play_encoded = play_encoded = True
 
-        # Description of CODEC2 Vocoder Codec2 encoder:
-        #   /*!
-        # * \brief CODEC2 Vocoder Encoder
-        # * \ingroup audio_blk
-        # *
-        # * Input: Speech (audio) signal as 16-bit shorts, sampling rate 8 kHz.
-        # *
-        # * Output: Vector of unpacked bits, forming one Codec2 frame, per 160
-        # *         input samples (in 2400 and 3200 bps modes) or per 320 input
-        # *         samples (in 1200, 1300, 1400 and 1600 bps modes).
-        # *
-        # */
-        # Taken from: https://www.gnuradio.org/doc/doxygen/codec2__encode__sp_8h_source.html
-        # Consequently the following buffer sizes are used in converting between the output and the bit stream:
-        match bit_rate:
+        match bit_rate: # for the reasoning behind the following buffer sizes check out https://www.gnuradio.org/doc/doxygen/codec2__encode__sp_8h_source.html
             case 3200:
                 self.bit_rate_mode = codec2.MODE_3200
                 self.buf_size = 64
@@ -132,21 +74,8 @@ class loopback_codec2(gr.top_block):
                 print('>>>> bit_rate value is missing or not supported.')
                 print('codec2.py [-e <audiofile.wav> samp_rate bit_rate <encodedfilename>] OR [-d <encodedfilename> samp_rate bit_rate <decodedaudiofile.wav>]')
                 exit()
-            
 
-
-        ##################################################
-        # Blocks
-        ##################################################
-        # _play_encoded_check_box = Qt.QCheckBox("Encode Audio")
-        # self._play_encoded_choices = {True: 1, False: 0}
-        # self._play_encoded_choices_inv = dict((v,k) for k,v in self._play_encoded_choices.items())
-        # self._play_encoded_callback = lambda i: Qt.QMetaObject.invokeMethod(_play_encoded_check_box, "setChecked", Qt.Q_ARG("bool", self._play_encoded_choices_inv[i]))
-        # self._play_encoded_callback(self.play_encoded)
-        # _play_encoded_check_box.stateChanged.connect(lambda i: self.set_play_encoded(self._play_encoded_choices[bool(i)]))
-        # self.top_layout.addWidget(_play_encoded_check_box)
-
-        if self.mode == 'd':
+        if self.mode == 'd': # decoding mode selected
             import pmt
             ##################################################
             # Blocks
@@ -179,7 +108,7 @@ class loopback_codec2(gr.top_block):
             self.connect((self.blocks_short_to_float_0, 0), (self.rational_resampler_xxx_1, 0))
             self.connect((self.rational_resampler_xxx_1, 0), (self.blocks_wavfile_sink_0, 0))
 
-        elif self.mode == 'e':
+        elif self.mode == 'e': # encoding mode selected
             ##################################################
             # Blocks
             self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
@@ -208,8 +137,6 @@ class loopback_codec2(gr.top_block):
             exit()
 
     def closeEvent(self, event):
-        # self.settings = Qt.QSettings("GNU Radio", "loopback_codec2")
-        # self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
 
@@ -249,28 +176,10 @@ def main(argv, top_block_cls=loopback_codec2, options=None):
         else: decoded = args[2]
         tb = top_block_cls('d', audio_file=arg, samp_rate=int(args[0]), bit_rate=int(args[1]), output_filename=decoded)
 
-    
     tb.run()
     qapp.exit()
-    # tb.start()
 
-    # tb.show()
 
-    # def sig_handler(sig=None, frame=None):
-    #     tb.stop()
-    #     tb.wait()
-
-    #     Qt.QApplication.quit()
-
-    # signal.signal(signal.SIGINT, sig_handler)
-    # signal.signal(signal.SIGTERM, sig_handler)
-
-    # # timer = Qt.QTimer()
-    # # timer.start(500)
-    # # timer.timeout.connect(lambda: None)
-
-    # qapp.exec_()
-    # tb.closeEvent()
 
 if __name__ == '__main__':
     main(sys.argv[1:])
